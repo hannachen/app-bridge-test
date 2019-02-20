c8602b57e5c20ad4e879a56cc7617ed388cf74c0
Hanna Chen <hanna.chen@shopify.com>
2019-02-20 16:39:52 -0500

    Put options directly inside `Modal.create()`

--- src/app.js
+++ src/app.js
@@ -1,17 +1,15 @@
 import createApp from "@shopify/app-bridge";
-import {Modal} from "@shopify/app-bridge/actions";
+import { Modal } from "@shopify/app-bridge/actions";
 
 const app = createApp({
   apiKey: "app_bridge_key",
   shopOrigin: "shop1.myshopify.io"
 });
 
-const modalOptions = {
+const myModal = Modal.create(app, {
   title: "My Modal",
   message: "Hello world!"
-};
-
-const myModal = Modal.create(app, modalOptions);
+});
 
 function openModal() {
   myModal.dispatch(Modal.Action.OPEN);
